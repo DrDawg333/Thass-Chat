ff3a1c40e7d39f4085c6b409ed60119be70ac4fb
Ashmit Thakur <146092693+DrDawg333@users.noreply.github.com>
2026-07-14 21:04:22 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -175,6 +175,8 @@ io.on("connection", async (socket) => {
 
 });
 
-server.listen(5000, () => {
-    console.log("🚀 Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
